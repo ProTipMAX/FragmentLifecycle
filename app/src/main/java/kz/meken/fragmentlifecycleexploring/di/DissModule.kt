908449a69ccff58b923f2bf70a8926cb16cec241
Meken <c2plusguru@gmail.com>
2020-11-29 00:09:47 +0600

    Delet 3 files

--- app/src/main/java/kz/meken/fragmentlifecycleexploring/di/DissModule.kt
+++
@@ -1,2 +0,0 @@
-package kz.meken.fragmentlifecycleexploring.di
-
--- app/src/main/java/kz/meken/fragmentlifecycleexploring/di/NetworkModule.kt
+++
@@ -1,8 +0,0 @@
-package kz.meken.fragmentlifecycleexploring.di
-
-import org.koin.core.module.Module
-import org.koin.dsl.module
-
-val networkModule: Module = module {
-
-}
